274de3ea99906a2b9300ed040a43e9a64605b8c4
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-01-29 16:08:15 +0200

    Update raport_oferta.py

--- raport_oferta.py
+++ raport_oferta.py
@@ -302,7 +302,7 @@ if st.session_state['file']!=None:
                 st.selectbox('Termen valabilitate',
                     range(1, 60),index=8, key='termen_val')
 
-  submitted = st.form_submit_button("Next")
+    submitted = st.form_submit_button("Next")
 
  # Logic AFTER the form
   if submitted:
