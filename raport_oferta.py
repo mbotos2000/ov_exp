d10b9ad6edf54ab91969e54425c7e47cc2b3b6fb
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-01-31 11:55:14 +0200

    Update raport_oferta.py

--- raport_oferta.py
+++ raport_oferta.py
@@ -13,6 +13,43 @@ from mailmerge import MailMerge
 from difflib import get_close_matches
 import pickle
 import string
+import streamlit_authenticator as stauth  # pip install streamlit-authenticator
+
+names = ["Peter Parker", "Rebecca Miller"]
+usernames = ["pparker", "rmiller"]
+ftp = ftplib.FTP_TLS("users.utcluj.ro")
+ftp.login(user=st.secrets['u'], passwd=st.secrets['p'])
+ftp.prot_p()
+ftp.encoding = "utf-8"  # Force UTF-8 encoding
+ftp.cwd('./public_html')
+# load hashed passwords
+file_name = "hashed_pw.pkl"
+file_data = io.BytesIO()
+# Download the file from the FTP server
+ftp.retrbinary(f'RETR {file_name}', file_data.write)
+# Seek to the beginning of the BytesIO object
+file_data.seek(0)
+            
+# Load the dictionary from the .pkl file
+hashed_passwords = pickle.load(file_data)
+      
+# Close the BytesIO object
+file_data.close()
+
+# Close the FTP connection
+ftp.quit()
+
+
+authenticator = stauth.Authenticate(names, usernames, hashed_passwords,
+    "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+name, authentication_status, username = authenticator.login("Login", "main")
+
+if authentication_status == False:
+    st.error("Username/password is incorrect")
+
+if authentication_status == None:
+    st.warning("Please enter your username and password")
 def float_to_eu(value: float) -> str:
     formatted = f"{value:,.2f}"
     return formatted.replace(",", "X").replace(".", ",").replace("X", ".")
