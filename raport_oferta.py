c3fe994b591e3caca25ab796e7a313c7e7e1ad05
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-01-31 15:58:08 +0200

    Update raport_oferta.py

--- raport_oferta.py
+++ raport_oferta.py
@@ -266,14 +266,14 @@ for key in ["val_inc_nd","nr_contract","data_contract","beneficiar","cerere","nu
 for key in ["zimax_et","zimin_et","zimax_a","zimin_a",
     "zimax_IND","zimin_IND","zimax_geo","zimin_geo","zimin_rel","zimax_rel","zimin_et_rel","zimax_et_rel"]:
     st.session_state.setdefault(key, int(60.0))
-keys_none=['cap2','cap3','cap4','resetare' ,'file']
+keys_none=['cap2','cap3','cap4','resetare' ,'file','cond']
 for key in keys_none:
     st.session_state.setdefault(key, None)
 
 
 st.session_state['file'] = st.file_uploader("Incarca centralizatorul in excel", type="xlsx")
-        
-if st.session_state['file']!=None or st.button("Nu am oferta in excell!"):
+st.session_state['cond']=st.button("Nu am oferta in excell!")
+if st.session_state['file']!=None or st.session_state['cond']!=None:
   if st.session_state['file']:
         df = pd.read_excel(st.session_state['file'], header=None)
         #st.dataframe(df)
