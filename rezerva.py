f188f377ad8d6023fcf69aa28a5e48d1ab93d661
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-02-03 11:36:34 +0200

    Create rezerva.py

---
+++ rezerva.py
@@ -0,0 +1,117 @@
+@st.cache_data
+def schimba_val_inc_nd(new):
+    st.session_state['val_inc_nd'] = str(new)
+@st.cache_data
+def schimba_nr_contract(new):
+    st.session_state['nr_contract'] = str(new)
+@st.cache_data
+def schimba_data_contract(new):
+    st.session_state['data_contract'] = str(new)
+@st.cache_data
+def schimba_beneficiar(new):
+    st.session_state['beneficiar'] = str(new)
+@st.cache_data
+def schimba_cerere(new):
+    st.session_state['cerere'] = str(new)
+@st.cache_data
+def schimba_numec(new):
+    st.session_state['numec'] = str(new)
+@st.cache_data
+def schimba_val_ET(new):
+    st.session_state['val_ET'] = str(new)
+@st.cache_data
+def schimba_ore_et(new):
+    st.session_state['ore_et'] = str(new)
+@st.cache_data
+def schimba_tarif_et(new):
+    st.session_state['tarif_et'] = str(new)
+@st.cache_data
+def schimba_zimax_et(new):
+    st.session_state['zimax_et'] = str(new)
+@st.cache_data
+def schimba_zimin_et(new):
+    st.session_state['zimin_et'] = str(new)
+@st.cache_data
+def schimba_val_a_3d(new):
+    st.session_state['val_a_3d'] = str(new)
+@st.cache_data
+def schimba_val_a_3d(new):
+    st.session_state['val_a_3d'] = str(new)
+@st.cache_data
+def schimba_val_a_rel(new):
+    st.session_state['val_a_rel'] = str(new)
+@st.cache_data
+def schimba_zimax_a(new):
+    st.session_state['zimax_a'] = str(new)
+@st.cache_data
+def schimba_zimin_a(new):
+    st.session_state['zimin_a'] = str(new)
+@st.cache_data
+def schimba_zimax_IND(new):
+    st.session_state['zimax_IND'] = str(new)
+@st.cache_data
+def schimba_zimin_IND(new):
+    st.session_state['zimin_IND'] = str(new)
+@st.cache_data
+def schimba_val_bet(new):
+    st.session_state['val_bet'] = str(new)    
+@st.cache_data
+def schimba_val_geo(new):
+    st.session_state['val_geo'] = str(new)
+
+@st.cache_data
+def schimba_val_geo(new):
+    st.session_state['val_geo'] = str(new)  
+
+@st.cache_data
+def schimba_val_dezveliri(new):
+    st.session_state['val_dezveliri'] = str(new) 
+
+
+@st.cache_data
+def schimba_nr_dezveliri(new):
+    st.session_state['nr_dezveliri'] = str(new)
+    
+@st.cache_data
+def schimba_zimax_geo(new):
+    st.session_state['zimax_geo'] = str(new)
+
+@st.cache_data
+def schimba_zimin_geo(new):
+    st.session_state['zimin_geo'] = str(new) 
+
+@st.cache_data
+def schimba_val_et_finisaje(new):
+    st.session_state['val_et_finisaje'] = str(new) 
+
+@st.cache_data
+def schimba_val_rel_struct(new):
+    st.session_state['val_rel_struct'] = str(new)
+
+@st.cache_data
+def schimba_val_et_actualizat(new):
+    st.session_state['val_et_actualizat'] = str(new)
+@st.cache_data
+def schimba_zimin_rel(new):
+    st.session_state['zimin_rel'] = str(new)
+@st.cache_data
+def schimba_zimax_rel(new):
+    st.session_state['zimax_rel'] = str(new)
+@st.cache_data
+def schimba_zimax_et_rel(new):
+    st.session_state['zimax_et_rel'] = str(new)
+@st.cache_data
+def schimba_zimin_et_rel(new):
+    st.session_state['zimin_et_rel'] = str(new)
+@st.cache_data
+def schimba_termen_predare(new):
+    st.session_state['zimin_et_rel'] = str(new)
+@st.cache_data
+def schimba_termen_predare(new):
+    st.session_state['termen_predare'] = str(new)
+@st.cache_data
+def schimba_termen_val(new):
+    st.session_state['termen_val'] = str(new)
+@st.cache_data
+def schimba_semnatura(new):
+    st.session_state['semnatura'] = str(new)
