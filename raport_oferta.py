b789f1d59815af5cfa7f9f9b1441c082018f6073
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2026-02-15 18:01:16 +0200

    Update raport_oferta.py

--- raport_oferta.py
+++ raport_oferta.py
@@ -440,8 +440,8 @@ if st.session_state['file']!=None or st.session_state['cond']!=None:
         r_c2=row["c2"]
         r_c3=str(row["c3"])
         r_c4=str(row["c4"])
-        st.session_state.cap3i+=f"\n\t     3.{idx+1} Elaborare releveu {r_c1} - {r_c2} RON + TVA;\n"
-        st.session_state.note+= f"\n\t     Termenul de elaborare pentru releveu {r_c1} este de maxim {r_c3} zile lucrătoare de la semnarea contractului și plata ratei 1, dar nu mai puțin de {r_c4} zile lucrătoare de la generarea norului de puncte.\n"
+        st.session_state.cap3i+=f"\t3.{idx+1} Elaborare releveu {r_c1} - {r_c2} RON + TVA;\n"
+        st.session_state.note+= f"\tTermenul de elaborare pentru releveu {r_c1} este de maxim {r_c3} zile lucrătoare de la semnarea contractului și plata ratei 1, dar nu mai puțin de {r_c4} zile lucrătoare de la generarea norului de puncte.\n"
         
 
       keys_to_merge=["val_inc_nd","val_ET","val_bet","val_geo","val_dezveliri","val_a_3d","val_a_rel", "val_et_finisaje","val_rel_struct","val_et_actualizat",
